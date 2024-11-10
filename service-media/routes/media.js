ba7836865eb98bff8f71e043f800bc2b376fef1e
Sofa <muhammadsofa52@yahoo.com>
2024-11-11 06:37:13 +0700

    Create API GET - List Image

--- service-media/routes/media.js
+++ service-media/routes/media.js
@@ -5,6 +5,24 @@ const base64Img = require('base64-img');
 
 const {Media} = require('../models');
 
+router.get('/',async(req, res) => {
+  //ambil semua gambar
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+
+  //set url/image
+  const mappedMedia = media.map(((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  }))
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  });
+});
+
 router.post('/', (req, res) => {
   // res.send('ok');
   const image = req.body.image;
@@ -22,7 +40,7 @@ router.post('/', (req, res) => {
     //'/public/images/1234.png'
     const filename = filepath.split('/').pop();
 
-    const media = await Media.create({image: `image/${filename}`});
+    const media = await Media.create({image: `images/${filename}`});
 
     return res.json({
       status: 'success',
