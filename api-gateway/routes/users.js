5bbdcb22b2a11abad87fd9dc62d2a61076005800
Sofa <muhammadsofa52@yahoo.com>
2024-11-25 11:03:16 +0700

    Integration API Update

--- api-gateway/routes/users.js
+++ api-gateway/routes/users.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const usersHandler = require('./handler/users');
+const verifyToken = require('../middlewares/verifyToken');
 //const {APP_NAME} = process.env;
 
 router.post('/register', usersHandler.register);
 router.post('/login', usersHandler.login);
+router.put('/', verifyToken, usersHandler.update);
 
 module.exports = router;
