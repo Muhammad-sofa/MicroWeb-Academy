703cfb8789469f577717296f01c0e8d03f1430e0
Sofa <muhammadsofa52@yahoo.com>
2024-12-02 13:13:23 +0700

    MyCourse - API Create

--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -32,3 +32,5 @@ Route::delete('lessons/{id}', 'App\Http\Controllers\LessonController@destroy');
 
 Route::post('image-courses', 'App\Http\Controllers\ImageCourseController@create');
 Route::delete('image-courses/{id}', 'App\Http\Controllers\ImageCourseController@destroy');
+
+Route::post('my-courses', 'App\Http\Controllers\MyCourseController@create');
