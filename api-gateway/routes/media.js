171aea3d1eb3b881898b092ecb5dfcb5ed931196
Sofa <muhammadsofa52@yahoo.com>
2024-12-05 16:10:01 +0700

    Add verifyToken

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+const verifyToken = require('../middlewares/verifyToken');
+
 const mediaHandler = require('./handler/media');
 //const {APP_NAME} = process.env;
 
 router.post('/', mediaHandler.create);
-router.get('/', mediaHandler.getAll);
+router.get('/',verifyToken, mediaHandler.getAll);
 router.delete('/:id', mediaHandler.destroy);
 
 module.exports = router;
