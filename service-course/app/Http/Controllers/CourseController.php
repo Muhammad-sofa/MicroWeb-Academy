cf530cdca51d9211cb0b8e2151d540dd897d253a
Sofa <muhammadsofa52@yahoo.com>
2024-12-05 12:32:15 +0700

    Course - Show Model Chapter, Mentor and Images

--- service-course/app/Http/Controllers/CourseController.php
+++ service-course/app/Http/Controllers/CourseController.php
@@ -37,7 +37,11 @@ class CourseController extends Controller
 
     public function show($id)
     {
-        $course = Course::find($id);
+        $course = Course::with('chapters.lessons')
+        ->with('mentor')
+        ->with('images')
+        ->find($id);
+
         if(!$course) {
             return response()->json([
                 'status'=> 'error',
