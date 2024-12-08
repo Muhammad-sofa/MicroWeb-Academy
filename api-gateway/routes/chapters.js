3806d8d0df75715cf0bcd610e4844850761db10d
Sofa <muhammadsofa52@yahoo.com>
2024-12-08 14:08:51 +0700

    API Chapter - Route

---
+++ api-gateway/routes/chapters.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const chaptersHandler = require('./handler/chapters');
+
+router.post('/', chaptersHandler.create);
+
+module.exports = router;
