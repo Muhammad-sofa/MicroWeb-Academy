591e1802f0c1f6eec28dd405226e2edf191e5ac1
Sofa <muhammadsofa52@yahoo.com>
2024-12-20 10:45:04 +0700

    Develop Service Order & Payment - Change MyCourse API Flow

--- service-order-payment/app/Models/helpers.php
+++ service-order-payment/app/Models/helpers.php
@@ -17,3 +17,19 @@ function createPremiumAccess($data) {
           ];
      }
 }
+
+function postOrder($params) {
+     $url = env("SERVICE_ORDER_PAYMENT_URL")."api/orders";
+     try {
+          $response = Http::post($url, $params);
+          $data = $response->json();
+          $data["http_code"] = $response->getStatusCode();
+          return $data;
+     } catch (\Throwable $e) {
+          return [
+               "status" => "error",
+               "http_code" => 500,
+               "message" => "service order payment unavailable"
+          ];
+     }
+}
